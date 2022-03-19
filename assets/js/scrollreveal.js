5dbc253cad678b208c923c6abb85f22c9135b0ee
Fran <los321gatoslocos@gmail.com>
2022-03-18 21:41:57 -0300

    project setup & some styles

---
+++ assets/js/scrollreveal.js
@@ -0,0 +1,14 @@
+
+
+// let node = document.querySelector('#texto1')
+
+// ScrollReveal().reveal(node, {
+//   duration: 2500,
+//   origin: 'left',
+//   distance: '-100px',
+
+// })
+
+console.log('ss')
+
+// ScrollReveal()
