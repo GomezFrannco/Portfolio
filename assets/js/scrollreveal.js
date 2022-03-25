a1a1829e11437fece7c514c3426a25ea5dbb55e6
Fran <los321gatoslocos@gmail.com>
2022-03-25 01:47:25 -0300

    update: scroll reveal setup

--- assets/js/scrollreveal.js
+++ assets/js/scrollreveal.js
@@ -1,14 +1,64 @@
+/**
+ *  Scroll Reveal Options
+ */
+var rigth = {
+  duration: 1500,
+  origin: 'rigth',
+  distance: '50px',
+  easing: 'ease-out',
+  reset: true
+}
+var left = {
+  duration: 1500,
+  origin: 'left',
+  distance: '50px',
+  easing: 'ease-out',
+  reset: true
+}
+var top = {
+  duration: 1500,
+  origin: 'top',
+  distance: '50px',
+  easing: 'ease-out',
+  reset: true
+}
+var bottom = {
+  duration: 1500,
+  origin: 'bottom',
+  distance: '50px',
+  easing: 'ease-out',
+  reset: true
+}
+
+/**
+ *  DOM Elements
+ */
+let titles = document.querySelectorAll('title');
+let node = document.querySelectorAll('.lft')
+let nd = document.querySelectorAll('.rgth')
+
+/**
+ *  Scroll Reveal Code 
+ */
+
+ScrollReveal().reveal(node, {
+  duration: 1500,
+  origin: 'rigth',
+  distance: '-40px',
+  easing: 'ease-out',
+  reset: true
+
+})
+
+ScrollReveal().reveal(nd, {
+  duration: 1500,
+  origin: 'top',
+  distance: '-50px',
+  easing: 'ease-out',
+  reset: true
+})
 
 
-// let node = document.querySelector('#texto1')
 
-// ScrollReveal().reveal(node, {
-//   duration: 2500,
-//   origin: 'left',
-//   distance: '-100px',
 
-// })
-
-console.log('ss')
-
-// ScrollReveal()
+ScrollReveal()
